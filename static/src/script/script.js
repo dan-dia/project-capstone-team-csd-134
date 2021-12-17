f05c34b4770e457451b23ca858c005a7261673f0
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-12-17 21:43:38 +0700

    Merubah modal bootstrap dengan sweetalert2 dan menambahkan according untuk table data

--- static/src/script/script.js
+++ static/src/script/script.js
@@ -1,5 +1,4 @@
 const form = document.getElementById('form');
-const modal = new bootstrap.Modal(document.getElementById('modalResult'));
 
 /* Button Predict */
 form.addEventListener('submit', function (e) {
@@ -42,51 +41,84 @@ form.addEventListener('submit', function (e) {
         .then(data => data.json())
         .then(res => {
             const { response } = res;
-            const modalBody = document.querySelector('.modal-body');
             const textModalBody = `
-      <p class="my-2 text-capitalize"><span class="fw-bold">Name</span> : ${name}</p>
-              <table class="table">
-                  <tbody>
-                      <tr>
-                          <th scope="row">Pregnancies</th>
-                          <td>${pregnancies}</td>
-                      </tr>
-                      <tr>
-                          <th scope="row">Glucose</th>
-                          <td>${glucose}</td>
-                      </tr>
-                      <tr>
-                          <th scope="row">Blood Pressure</th>
-                          <td>${bloodPressure}</td>
-                      </tr>
-                      <tr>
-                          <th scope="row">Skin Thickness</th>
-                          <td>${skinThickness}</td>
-                      </tr>
-                      <tr>
-                          <th scope="row">Insulin</th>
-                          <td>${insulin}</td>
-                      </tr>
-                      <tr>
-                          <th scope="row">BMI</th>
-                          <td>${bmi}</td>
-                      </tr>
-                      <tr>
-                          <th scope="row">Diabetes Pedigree Function</th>
-                          <td>${diabetesPedigreeFunction}</td>
-                      </tr>
-                      <tr>
-                          <th scope="row">Age</th>
-                          <td>${age}</td>
-                      </tr>
-                  </tbody>
-              </table>
-              <div class="text-center">
-                  <p>Berdasarkan data diatas anda dinyatakan : </p>
-                  <p><span class="fw-bold text-uppercase">${response}</span> terhadap penyakit diabetes.</p>
-              </div>`;
-            modalBody.innerHTML = textModalBody;
-            modal.show();
+            <p class="my-2 text-capitalize text-start"><span class="fw-bold">Name</span> : ${name}</p>
+            <div class="accordion m-3" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        Data
+                    </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <table class="table table-sm text-start">
+                                <tbody>
+                                    <tr>
+                                        <th>Pregnancies</th>
+                                        <td>${pregnancies}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Glucose</th>
+                                        <td>${glucose}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Blood Pressure</th>
+                                        <td>${bloodPressure}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Skin Thickness</th>
+                                        <td>${skinThickness}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Insulin</th>
+                                        <td>${insulin}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>BMI</th>
+                                        <td>${bmi}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Diabetes Pedigree Function</th>
+                                        <td>${diabetesPedigreeFunction}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Age</th>
+                                        <td>${age}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>Berdasarkan data, Sdri <span class="text-capitalize">${name}</span> dinyatakan : </p>
+                <p><span class="fw-bold text-uppercase">${response}</span> terhadap Penyakit Diabetes.</p>
+            </div>`;
+
+            let colorBackdrop = '';
+            let colorIcon = '';
+
+            if (response === 'negatif') {
+                colorBackdrop = 'rgba(25, 135, 84, .6)';
+                colorIcon = '#198754';
+            } else {
+                colorBackdrop = 'rgba(220, 53, 69, .6)';
+                colorIcon = '#dc3545';
+            }
+
+            Swal.fire({
+                title: `<strong>Result</strong>`,
+                icon: 'info',
+                html: textModalBody,
+                showCancelButton: true,
+                showConfirmButton: false,
+                focusCancel: true,
+                cancelButtonText: 'Close',
+                backdrop: colorBackdrop,
+                iconColor: colorIcon
+              })
         })
         .catch(m => console.log(m));
 });
