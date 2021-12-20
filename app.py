af49eec7229789387255e8113e7b0882a2328653
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-12-20 15:16:32 +0700

    Menambahkan route untuk diabetes dan bmi

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import pickle
-import pandas as pd
+# import pandas as pd
 import numpy as np
 # from sklearn.preprocessing import StandardScaler
 
@@ -10,7 +10,19 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html.jinja')
 
-@app.route('/process_to_model', methods=['POST', 'GET'])
+@app.route("/diabetes")
+def diabetes(data=None):
+  return render_template('diabetes.html.jinja', data={
+    'title': '| Diabetes'
+  })
+
+@app.route("/bmi")
+def bmi(data=None):
+  return render_template('bmi.html.jinja', data={
+    'title': '| BMI'
+  })
+
+@app.route('/process_to_model', methods=['POST'])
 def process_to_model():
   if request.method == "POST":
     diabetes = request.get_json()
